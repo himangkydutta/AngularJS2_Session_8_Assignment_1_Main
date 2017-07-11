8cda853bbb8db338c0c622953784c901318e33f7
himangkydutta <himangkydutta08@gmail.com>
2017-07-12 00:28:21 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { LoginComponent } from './app.component';
+import {EmployeeComponent} from './employee-app.component';
+import {AppComponent} from './person-app.component';
+
+@NgModule({
+  declarations: [
+    EmployeeComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [EmployeeComponent]
+})
+export class AppModule { }
